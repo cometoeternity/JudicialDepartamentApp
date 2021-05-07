6b55e59f739c1470199127dbeadcbf5e2749c831
Дмитрий Павлов <dmitriyeternitypavlov@outlook.com>
2021-05-07 16:03:33 +0700

    Создание инфраструктуры приложения, базового класса модели-представления.

---
+++ JudicialDepartamentApp/Models/Document.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JudicialDepartamentApp.Model
+{
+    public class Document
+    {
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+        public string Author { get; set; }
+        public string DocumentType { get; set; }
+        public byte[] BinaryFile { get; set; } 
+    }
+}
