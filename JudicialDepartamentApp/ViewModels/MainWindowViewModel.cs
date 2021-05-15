b664a8736648d700e5f5dc2d98c448ac5768a01f
Дмитрий Павлов <dmitriyeternitypavlov@outlook.com>
2021-05-15 18:42:13 +0700

    Создание главного меню приложения и функциональных конопок

--- JudicialDepartamentApp/ViewModels/MainWindowViewModel.cs
+++ JudicialDepartamentApp/ViewModels/MainWindowViewModel.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using JudicialDepartamentApp.Infrastructure.Commands;
 using JudicialDepartamentApp.ViewModels.Base;
 
 namespace JudicialDepartamentApp.ViewModels
@@ -14,5 +17,17 @@ namespace JudicialDepartamentApp.ViewModels
         /// Заголовок окна.
         /// </summary>
         public string Title { get => _Title; set => Set(ref _Title, value); }
+
+        public ICommand CloseApplicationCommand { get; }
+        private bool CanCloseApplicationCommandExecuted(object p) => true;
+        private void OnCloseApplicationCommandExecuted(object p)
+        {
+            Application.Current.Shutdown(); 
+        }
+
+        public MainWindowViewModel()
+        {
+            CloseApplicationCommand = new LambdaCommand(OnCloseApplicationCommandExecuted, CanCloseApplicationCommandExecuted);
+        }
     }
 }
